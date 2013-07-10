1ccafdbe917583d78db5900e8424bedf76c11c35
Ubuntu <ubuntu@ip-172-31-38-123.us-west-2.compute.internal>
2013-07-10 09:40:06 +0000

    grader modified1

--- grader.js
+++ grader.js
@@ -21,11 +21,33 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var util = require('util');
+var rest = require('restler'); 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTMLFILE_TMP = "tmpindex.html";
+
+var buildfn = function(htmlfile) {
+    var response2console = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            console.error("Wrote %s", htmlfile);
+            fs.writeFileSync(htmlfile, result);
+        }
+    };
+    return response2console;
+};
+
+var buildUrl = function(inurl) {
+    var instr = inurl;
+    var response2console = buildfn(HTMLFILE_TMP);
+    rest.get(instr).on('complete', response2console);
+    return HTMLFILE_TMP;  
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,8 +87,17 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_file_url>', 'Url to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    var htmlfile="";
+    if(program.url) {
+	htmlfile = buildUrl(program.url);
+    } else { 
+	htmlfile = program.file;
+    }
+
+    var checkJson = checkHtmlFile(htmlfile, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
